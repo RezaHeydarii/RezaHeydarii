b69fddefa6d8f5fbeb9fc1fa2f76300dd71a9216
RezaHeydarii <r.heydarii98@gmail.com>
2025-06-12 00:25:31 +0330

    build(app): config added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/rezaheydarii",
+  assetPrefix: "/rezaheydarii/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
